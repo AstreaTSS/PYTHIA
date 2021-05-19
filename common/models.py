e5d7d3994d45c4305ad27046386d50537f55529d
Sonic4999 <sonictbm@gmail.com>
2021-05-19 15:41:48 -0400

    Minor fix to saving sets to dbs

--- common/models.py
+++ common/models.py
@@ -32,7 +32,10 @@ class SetField(fields.BinaryField, set):
         if value is not None and not isinstance(
             value, self.field_type
         ):  # if its not bytes
-            value = self.json_dumps(value)  # returns a bytes value
+            if isinstance(value, set):  # this is a dumb fix
+                value = self.json_dumps(list(value))  # returns a bytes value
+            else:
+                value = self.json_dumps(value)
             # the reason why i chose using BinaryField over JSONField
             # was because orjson returns bytes, and orjson's fast
         return value
