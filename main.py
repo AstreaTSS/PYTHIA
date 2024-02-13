cddb58031a08fc6ee68d8f75e2cc5a0c816c8f62
AstreaTSS <25420078+AstreaTSS@users.noreply.github.com>
2024-02-12 19:54:21 -0500

    feat: add ! to status

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ class UltimateInvestigator(utils.UIBase):
         activity = ipy.Activity(
             name="Splash Text",
             type=ipy.ActivityType.CUSTOM,
-            state="Watching for Truth Bullets",
+            state="Watching for Truth Bullets!",
         )
         await self.change_presence(activity=activity)
 
@@ -66,7 +66,7 @@ class UltimateInvestigator(utils.UIBase):
         activity = ipy.Activity(
             name="Splash Text",
             type=ipy.ActivityType.CUSTOM,
-            state="Watching for Truth Bullets",
+            state="Watching for Truth Bullets!",
         )
         await self.change_presence(activity=activity)
 
