d46e25cf8c3c888a8853b7764ce286842101a280
AstreaTSS <25420078+AstreaTSS@users.noreply.github.com>
2024-02-12 19:00:21 -0500

    docs: add copyright to license header

--- exts/help_cmd.py
+++ exts/help_cmd.py
@@ -1,4 +1,13 @@
 """
+Copyright 2022-2024 AstreaTSS.
+This file is part of Ultimate Investigator.
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+"""
+
+"""
 This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/.
