ad9ef076b534d48d730c5c541c34bb1db4c2c2d3
AstreaTSS <25420078+AstreaTSS@users.noreply.github.com>
2024-07-04 21:49:52 -0400

    fix: make cutoff a bit bigger

---
+++ migrations/20240613000737_names_rearrange/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+  - The primary key for the `thianames` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- DropForeignKey
+ALTER TABLE "thiaguildconfig" DROP CONSTRAINT "thiaguildconfig_names_id_fkey";
+
+-- DropIndex
+DROP INDEX "thiaguildconfig_names_id_key";
+
+-- AlterTable
+ALTER TABLE "thiabulletconfig" ALTER COLUMN "guild_id" DROP DEFAULT;
+DROP SEQUENCE "uinewconfig_guild_id_seq";
+
+ALTER TABLE "thianames" RENAME "id" TO "guild_id";
+
+-- AlterTable
+ALTER TABLE "thianames" DROP CONSTRAINT "thianames_pkey",
+ALTER COLUMN "guild_id" DROP DEFAULT,
+ALTER COLUMN "guild_id" SET DATA TYPE BIGINT,
+ADD CONSTRAINT "thianames_pkey" PRIMARY KEY ("guild_id");
+DROP SEQUENCE "PrismaNames_id_seq";
+
+-- InsertTable
+UPDATE thianames SET guild_id = g.guild_id FROM thiaguildconfig g WHERE thianames.guild_id = g.names_id;
+DELETE FROM thianames WHERE guild_id < 10000;  -- this just happens?
+
+-- AlterTable
+ALTER TABLE "thiaguildconfig" DROP COLUMN "names_id";
+
+-- RenameForeignKey
+ALTER TABLE "thiaguildconfig" RENAME CONSTRAINT "thiaguildconfig_guild_id_fkey" TO "thiaguildconfig_guild_id_bullets_fkey";
