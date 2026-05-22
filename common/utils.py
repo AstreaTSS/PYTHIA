1bdcec37bbc45fcc1cb2de061b0f2548f98f9b84
Astrea <25420078+AstreaTSS@users.noreply.github.com>
2026-05-21 22:34:43 -0400

    feat💥: start work on pycord rewrite
    Work is being done incrementally, and all commits will be squashed at the end.

--- common/utils.py
+++ common/utils.py
@@ -7,196 +7,35 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/.
 """
 
-import functools
 import logging
 import os
 import traceback
-from copy import copy
 from pathlib import Path
 
 import aiohttp
-import interactions as ipy
-import modal_backport as modalb
+import discord
 import sentry_sdk
-import tansy
 import typing_extensions as typing
-from interactions.ext import hybrid_commands as hybrid
-from interactions.ext import prefixed_commands as prefixed
-from tansy.slash_commands import tansy_parse_parameters
 
-import common.models as models
+from common.core import *
 
 OS_TRUE_VALUES = frozenset({"true", "True", "TRUE", "t", "T", "1"})
 SENTRY_ENABLED = bool(os.environ.get("SENTRY_DSN", False))  # type: ignore
 VOTING_ENABLED = bool(os.environ.get("TOP_GG_TOKEN") or os.environ.get("DBL_TOKEN"))
 DOCKER_ENABLED = os.environ.get("DOCKER_MODE") in OS_TRUE_VALUES
-BOT_COLOR = ipy.Color(int(os.environ["BOT_COLOR"]))
+BOT_COLOR = discord.Color(int(os.environ["BOT_COLOR"]))
 MAX_DICE_ENTRIES: typing.Final[int] = 50
 
-logger = logging.getLogger("pythiabot")
+logger = logging.getLogger("discord")
 
 
-SlashCommandT = typing.TypeVar("SlashCommandT", bound=ipy.SlashCommand)
+def parse_hex_number(hex_number: str) -> discord.Color:
+    if hex_number.startswith("#"):
+        hex_number = hex_number[1:]
 
-
-@functools.wraps(tansy.slash_command)
-def manage_guild_slash_cmd(
-    name: str,
-    description: ipy.Absent[str] = ipy.MISSING,
-) -> typing.Callable[[ipy.const.AsyncCallable], tansy.TansySlashCommand]:
-    return tansy.slash_command(
-        name=name,
-        description=description,
-        default_member_permissions=ipy.Permissions.MANAGE_GUILD,
-    )
-
-
-def _generate_parse_parameters(
-    command: tansy.TansySlashCommand,
-    other_cmd: tansy.TansySlashCommand,
-) -> typing.Callable[[], None]:
-    def _parse_parameters() -> None:
-        if other_cmd._inspect_signature:
-            command._inspect_signature = other_cmd._inspect_signature
-        if other_cmd.options:
-            command.options = other_cmd.options
-        tansy_parse_parameters(command)
-
-    return _parse_parameters
-
-
-def alias(
-    command: SlashCommandT,
-    name: str,
-    description: str,
-    *,
-    base_command: ipy.SlashCommand | None = None,
-) -> SlashCommandT:
-    alias = copy(command)
-
-    if base_command:
-        alias.description = base_command.description
-        alias.dm_permission = base_command.dm_permission
-        alias.default_member_permissions = base_command.default_member_permissions
-        alias.scopes = base_command.scopes
-        alias.integration_types = base_command.integration_types
-        alias.contexts = base_command.contexts
-
-    names = name.split()
-
-    if len(names) == 1:
-        alias.name = name
-        alias.description = description
-    elif len(names) == 2:
-        alias.name = names[0]
-        alias.sub_cmd_name = names[1]
-        alias.sub_cmd_description = description
-    else:
-        alias.name = names[0]
-        alias.group_name = names[1]
-        alias.sub_cmd_name = names[2]
-        alias.sub_cmd_description = description
-
-    # i heard you like references
-    # here we're abusing them so editing the original version's attributes
-    # also affects the alias, which is nice
-    alias.checks = command.checks
-    alias.options = command.options
-    alias.autocomplete_callbacks = command.autocomplete_callbacks
-
-    if isinstance(command, tansy.TansySlashCommand):
-        alias.parameters = command.parameters
-        alias._parse_parameters = _generate_parse_parameters(alias, command)
-        command._parse_parameters = _generate_parse_parameters(command, alias)
-    else:
-        alias.parameters = command.parameters.copy()
-
-    return alias
-
-
-def error_embed_generate(error_msg: str) -> ipy.Embed:
-    return ipy.Embed(
-        description=f"# Error\n{error_msg}",
-        color=ipy.MaterialColors.ORANGE,
-    )
-
-
-def make_embed(description: str, *, title: str | None = None) -> ipy.Embed:
-    return ipy.Embed(
-        description=description if not title else f"# {title}\n{description}",
-        color=BOT_COLOR,
-    )
-
-
-async def error_handle(error: Exception, *, ctx: ipy.BaseContext | None = None) -> None:
-    if not isinstance(error, aiohttp.ServerDisconnectedError):
-        if SENTRY_ENABLED:
-            scope = sentry_sdk.Scope.get_current_scope()
-            if ctx:
-                scope.set_context(
-                    type(ctx).__name__,
-                    {
-                        "args": ctx.args,  # type: ignore
-                        "kwargs": ctx.kwargs,  # type: ignore
-                        "message": ctx.message,
-                    },
-                )
-            sentry_sdk.capture_exception(error)
-        else:
-            traceback.print_exception(error)
-            logger.error("An error occured.", exc_info=error)
-    if ctx:
-        if isinstance(ctx, prefixed.PrefixedContext):
-            await ctx.reply(
-                embed=error_embed_generate(
-                    "An internal error has occured. The bot owner has been notified "
-                    "and will likely fix the issue soon."
-                )
-            )
-        elif isinstance(ctx, ipy.InteractionContext):
-            await ctx.send(
-                embed=error_embed_generate(
-                    "An internal error has occured. The bot owner has been notified "
-                    "and will likely fix the issue soon."
-                ),
-                ephemeral=ctx.ephemeral,
-            )
-
-
-async def msg_to_owner(
-    bot: "THIABase",
-    chunks: list[str] | list[ipy.Embed] | list[str | ipy.Embed] | str | ipy.Embed,
-) -> None:
-    if not isinstance(chunks, list):
-        chunks = [chunks]
-
-    # sends a message to the owner
-    for chunk in chunks:
-        if isinstance(chunk, ipy.Embed):
-            await bot.owner.send(embeds=chunk)
-        else:
-            await bot.owner.send(chunk)
-
-
-def line_split(content: str, split_by: int = 20) -> list[list[str]]:
-    content_split = content.splitlines()
-    return [
-        content_split[x : x + split_by] for x in range(0, len(content_split), split_by)
-    ]
-
-
-def deny_mentions(user: ipy.Snowflake_Type) -> ipy.AllowedMentions:
-    # generates an AllowedMentions object that only pings the user specified
-    return ipy.AllowedMentions(users=[user])
-
-
-def error_format(error: Exception) -> str:
-    # simple function that formats an exception
-    return "".join(
-        traceback.format_exception(  # type: ignore
-            type(error), value=error, tb=error.__traceback__
-        )
-    )
+    arg = "".join(i * 2 for i in hex_number) if len(hex_number) == 3 else hex_number
+    value = int(arg, base=16)
+    return discord.Color(value=value)
 
 
 def file_to_ext(str_path: str, base_path: str) -> str:
@@ -228,214 +67,64 @@ def get_all_extensions(str_path: str, folder: str = "exts") -> list[str]:
     return ext_files
 
 
-def toggle_friendly_str(bool_to_convert: bool) -> typing.Literal["on", "off"]:
-    return "on" if bool_to_convert else "off"
-
-
-def yesno_friendly_str(bool_to_convert: bool) -> typing.Literal["yes", "no"]:
-    return "yes" if bool_to_convert else "no"
-
-
-def convert_to_bool(argument: str) -> bool:
-    lowered = argument.lower()
-    if lowered in {"yes", "y", "true", "t", "1", "enable", "on"}:
-        return True
-    if lowered in {"no", "n", "false", "f", "0", "disable", "off"}:
-        return False
-    raise ipy.errors.BadArgument(f"{argument} is not a recognised boolean option.")
-
-
-def partial_channel(bot: "THIABase", channel_id: ipy.Snowflake_Type) -> ipy.GuildText:
-    return ipy.GuildText(
-        client=bot, id=ipy.to_snowflake(channel_id), type=ipy.ChannelType.GUILD_TEXT
-    )  # type: ignore
-
-
-def role_check(ctx: ipy.BaseContext, role: ipy.Role) -> ipy.Role:
-    top_role = ctx.guild.me.top_role
-
-    if role > top_role:
-        raise CustomCheckFailure(
-            "The role provided is a role that is higher than the roles I can edit. "
-            + "Please move either that role or my role so that "
-            + "my role is higher than the role you want to use."
-        )
-
-    return role
-
-
-AsyncT = typing.TypeVar("AsyncT", bound=ipy.const.AsyncCallable)
-
-
-def modal_event_error_handler(func: AsyncT) -> AsyncT:
-    async def wrapper(
-        self: typing.Any,
-        unknown: ipy.events.ModalCompletion,
-        *args: typing.Any,
-        **kwargs: typing.Any,
-    ) -> None:
-        ctx = unknown.ctx
-
-        try:
-            await func(self, unknown, *args, **kwargs)
-        except ipy.errors.BadArgument as e:
-            await ctx.send(embeds=error_embed_generate(str(e)), ephemeral=True)
-        except Exception as e:
-            await error_handle(e, ctx=ctx)
-
-    return wrapper  # type: ignore
-
-
-class ValidRoleConverter(ipy.Converter):
-    async def convert(
-        self, context: ipy.InteractionContext, argument: ipy.Role
-    ) -> ipy.Role:
-        return role_check(context, argument)
-
-
-class CustomCheckFailure(ipy.errors.BadArgument):
-    # custom classs for custom prerequisite failures outside of normal command checks
-    pass
-
-
-class GuildMessageable(ipy.GuildChannel, ipy.MessageableMixin):
-    pass
-
-
-def valid_channel_check(
-    channel: ipy.GuildChannel, perms: ipy.Permissions
-) -> GuildMessageable:
-    if not isinstance(channel, ipy.MessageableMixin):
-        raise ipy.errors.BadArgument(f"Cannot send messages in {channel.name}.")
-
-    if not perms:
-        raise ipy.errors.BadArgument(f"Cannot resolve permissions for {channel.name}.")
-
-    if (
-        ipy.Permissions.VIEW_CHANNEL not in perms
-    ):  # technically pointless, but who knows
-        raise ipy.errors.BadArgument(f"Cannot read messages in {channel.name}.")
-    elif ipy.Permissions.READ_MESSAGE_HISTORY not in perms:
-        raise ipy.errors.BadArgument(f"Cannot read message history in {channel.name}.")
-    elif ipy.Permissions.SEND_MESSAGES not in perms:
-        raise ipy.errors.BadArgument(f"Cannot send messages in {channel.name}.")
-    elif ipy.Permissions.EMBED_LINKS not in perms:
-        raise ipy.errors.BadArgument(f"Cannot send embeds in {channel.name}.")
-
-    return channel  # type: ignore
-
-
-class ValidChannelConverter(ipy.Converter):
-    async def convert(
-        self, ctx: ipy.InteractionContext, argument: ipy.GuildText
-    ) -> GuildMessageable:
-        perms = argument.permissions
-        if perms is None:
-            perms = ctx.app_permissions
-
-        return valid_channel_check(argument, perms)
-
-
-async def _global_checks(ctx: ipy.BaseContext) -> bool:
-    return bool(ctx.guild) if ctx.bot.is_ready else False
-
-
-class Extension(ipy.Extension):
-    bot: "THIABase"
-
-    def __new__(
-        cls, bot: ipy.Client, *args: typing.Any, **kwargs: typing.Any
-    ) -> "typing.Self":
-        new_cls = super().__new__(cls, bot, *args, **kwargs)
-        new_cls.add_ext_check(_global_checks)
-        return new_cls
-
-
-if typing.TYPE_CHECKING:
-    import asyncio
-    import collections
-
-    from interactions.ext.prefixed_commands import PrefixedManager
-
-    from .help_tools import MiniCommand, PermissionsResolver
-
-    class THIABase(ipy.AutoShardedClient):
-        prefixed: PrefixedManager
-        hybrid: hybrid.HybridManager
-        owner: ipy.User
-        color: ipy.Color
-        background_tasks: set[asyncio.Task]
-        slash_perms_cache: collections.defaultdict[int, dict[int, PermissionsResolver]]
-        mini_commands_per_scope: dict[int, dict[str, MiniCommand]]
-        msg_enabled_bullets_guilds: set[int]
-        gacha_locks: collections.defaultdict[str, asyncio.Lock]
-
-        def create_task(self, coro: typing.Coroutine) -> asyncio.Task: ...
-
-else:
-
-    class THIABase(ipy.AutoShardedClient):
+def quick_view(
+    *items: discord.ui.ViewItem,
+) -> discord.ui.View:
+    class CustomView(discord.ui.View):
         pass
 
-
-class THIAContextMixin:
-    guild_config: models.GuildConfig | None
-    guild_id: ipy.Snowflake
-
-    def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
-        self.guild_config = None
-        super().__init__(*args, **kwargs)
-
-    @property
-    def guild(self) -> ipy.Guild:
-        return self.client.cache.get_guild(self.guild_id)  # type: ignore
-
-    @property
-    def bot(self) -> "THIABase":
-        """A reference to the bot instance."""
-        return self.client  # type: ignore
-
-    async def fetch_config(
-        self,
-        include: models.GuildConfigInclude | None = None,
-    ) -> models.GuildConfig:
-        """
-        Gets the configuration for the context's guild.
-
-        Returns:
-            The guild config.
-        """
-        if self.guild_config:
-            return self.guild_config
-
-        config = await models.GuildConfig.fetch_create(self.guild_id, include)
-        self.guild_config = config
-        return config
+    return CustomView(*items, store=False)
 
 
-class THIABaseContext(THIAContextMixin, ipy.BaseContext):
-    pass
+def quick_designer_view(
+    *items: discord.ui.ViewItem,
+) -> discord.ui.DesignerView:
+    class CustomDesignerView(discord.ui.DesignerView):
+        pass
+
+    return CustomDesignerView(*items, store=False)
 
 
-class THIAModalContext(THIAContextMixin, modalb.ModalContext):
-    pass
+def error_view_generate(error_msg: str) -> discord.ui.DesignerView:
+    return quick_designer_view(
+        discord.ui.Container(
+            discord.ui.TextDisplay(f"# Error\n{error_msg}"),
+            color=parse_hex_number("#FF9800"),
+        )
+    )
 
 
-class THIAInteractionContext(THIAContextMixin, ipy.InteractionContext):
-    pass
+async def error_handle(
+    error: Exception, *, ctx: THIABridgeContext | discord.Interaction | None = None
+) -> None:
+    if not isinstance(error, aiohttp.ServerDisconnectedError):
+        if SENTRY_ENABLED:
+            scope = sentry_sdk.Scope.get_current_scope()
+            if ctx:
+                scope.set_context(
+                    type(ctx).__name__,
+                    {
+                        "args": ctx.args,  # type: ignore
+                        "kwargs": ctx.kwargs,  # type: ignore
+                        "message": ctx.message,
+                    },
+                )
+            sentry_sdk.capture_exception(error)
+        else:
+            traceback.print_exception(error)
+            logger.error("An error occured.", exc_info=error)
+    if ctx and isinstance(
+        ctx,
+        (THIABridgeApplicationContext, THIABridgeExtContext, discord.Interaction),
+    ):
+        await ctx.respond(
+            view=error_view_generate(
+                "An internal error has occured. The bot owner has been notified and"
+                " will likely fix the issue soon."
+            )
+        )
 
 
-class THIAComponentContext(THIAContextMixin, ipy.ComponentContext):
-    pass
-
-
-class THIASlashContext(THIAContextMixin, ipy.SlashContext):
-    pass
-
-
-class THIAHybridContext(THIAContextMixin, hybrid.HybridContext):
-    pass
-
-
-class THIAPrefixedContext(THIAContextMixin, prefixed.PrefixedContext):
+class CustomCheckFailure(discord.CheckFailure):
+    # custom classs for custom prerequisite failures outside of normal command checks
     pass
