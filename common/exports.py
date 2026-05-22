95447553cb75fbe6bb785c2f675ba95cb95b6bb9
Astrea <25420078+AstreaTSS@users.noreply.github.com>
2026-05-21 23:09:33 -0400

    feat: re-add common/exports.py
    This file needs no new changes to function

---
+++ common/exports.py
@@ -0,0 +1,86 @@
+"""
+Copyright 2021-2026 AstreaTSS.
+This file is part of PYTHIA.
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+"""
+
+import typing
+
+import msgspec
+
+
+class GachaItemv1(msgspec.Struct):
+    name: str
+    description: str
+    amount: int
+    image: str | None = None
+
+
+class GachaItemv2(msgspec.Struct):
+    name: str
+    description: str
+    rarity: int
+    amount: int
+    image: str | None = None
+
+
+class GachaItemDict(typing.TypedDict):
+    name: str
+    description: str
+    rarity: int
+    amount: int
+    image: str | None
+
+
+class DiceEntryv1(msgspec.Struct):
+    name: str
+    value: str
+
+
+class DiceEntryDict(typing.TypedDict):
+    name: str
+    value: str
+
+
+class GachaItemv1Container(msgspec.Struct, tag=1, tag_field="version"):
+    items: list[GachaItemv1]
+
+
+class GachaItemv2Container(msgspec.Struct, tag=2, tag_field="version"):
+    items: list[GachaItemv2]
+
+
+class DiceEntryv1Container(msgspec.Struct, tag=1, tag_field="version"):
+    entries: list[DiceEntryv1]
+
+
+GachaItemContainer = GachaItemv1Container | GachaItemv2Container
+gacha_dec = msgspec.json.Decoder(GachaItemContainer)
+dice_dec = msgspec.json.Decoder(DiceEntryv1Container)
+
+
+def handle_gacha_item_data(json_data: str | bytes) -> list[GachaItemv2]:
+    container: GachaItemContainer = gacha_dec.decode(json_data)
+
+    if isinstance(container, GachaItemv1Container):
+        items = [
+            GachaItemv2(
+                name=item.name,
+                description=item.description,
+                rarity=1,  # default rarity for v1 items - common
+                amount=item.amount,
+                image=item.image,
+            )
+            for item in container.items
+        ]
+        container = GachaItemv2Container(items=items)
+
+    return container.items
+
+
+def handle_dice_entry_data(json_data: str | bytes) -> list[DiceEntryv1]:
+    container: DiceEntryv1Container = dice_dec.decode(json_data)
+    return container.entries
